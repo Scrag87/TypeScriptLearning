9bc92fb1eb2028f6e1c62be408c44aa964bd0817
scrag <scrag1987@gmail.com>
2019-07-26 17:45:57 +0600

    script fill

---
+++ script.ts
@@ -0,0 +1,30 @@
+class Car {
+    model: String;
+    doors: Number;
+    isElectric: Boolean;
+    carId: string;
+
+    constructor(model: String, doors: Number, isElectric: Boolean, carId: string) {
+        this.model = model;
+        this.doors = doors;
+        this.isElectric = isElectric;
+        this.carId = carId;
+
+    }
+
+    make(): void {
+        let x: string;
+        let carId: string;
+        carId = this.carId;
+        x = "";
+        let car = document.getElementById(carId);
+        x = car.textContent;
+        car.innerText = x + ` This car is ${this.model} which has ${this.doors} doors`;
+        car.style.background = "orange";
+    }
+}
+
+let car2 = new Car("Innova", 5, false, "car2");
+let car1 = new Car("bmv", 5, true, "car1");
+car2.make();
+car1.make();
