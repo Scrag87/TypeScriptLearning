5acc1336faf6e67addf0d42a8f60157095c80e89
scrag <scrag1987@gmail.com>
2019-07-26 17:53:07 +0600

    ts1st run

---
+++ script.js
@@ -0,0 +1,24 @@
+var Car = /** @class */ (function () {
+    function Car(model, doors, isElectric, carId) {
+        this.model = model;
+        this.doors = doors;
+        this.isElectric = isElectric;
+        this.carId = carId;
+    }
+    Car.prototype.make = function () {
+        var x;
+        var carId;
+        carId = this.carId;
+        x = "";
+        var car = document.getElementById(carId);
+        x = car.textContent;
+        car.innerText = x + (" This car is " + this.model + " which has " + this.doors + " doors");
+        car.style.background = "orange";
+    };
+    return Car;
+}());
+var car2 = new Car("Innova", 5, false, "car2");
+var car1 = new Car("bmv", 5, true, "car1");
+car2.make();
+car1.make();
+//# sourceMappingURL=script.js.map
