b8bfce78571d406492afc1733d7723772f308eaa
scrag <scrag1987@gmail.com>
2019-07-29 16:26:46 +0600

    end result

--- TypeScript/DOMoperations.ts
+++ TypeScript/DOMoperations.ts
@@ -1,36 +1,54 @@
-class newTxtElem {
-  color: string;
-  idE: string;
-  text: string;
-  type: string;
-  ParentId: string;
+class newElem {
+    idE: string;
+    type: string;
+    parentId: string;
 
-  constructor(
-    type: string,
-    idE: string,
-    text: string,
-    color: string,
-    parentId: string
-  ) {
-    this.color = color;
-    this.idE = idE;
-    this.ParentId = parentId;
-    this.text = text;
-    this.type = type;
-  }
+    constructor(type: string, idE: string, parentId: string) {
+        this.idE = idE;
+        this.parentId = parentId;
+        this.type = type;
+    }
 
-  makeElem(): void {
-    let para = document.createElement(this.type);
-    let node = document.createTextNode(this.text);
-    para.appendChild(node);
-    para.style.background = this.color;
-    para.id = this.idE;
-    let element = document.getElementById(this.ParentId);
-    element.appendChild(para);
-  }
+    paintToColor(color: string) {
+        let element = document.getElementById(this.idE);
+        element.style.background = color;
+    }
+
+    makeElem(type: string, idE: string, parentId: string) {
+        let para = document.createElement(this.type);
+        para.id = this.idE;
+        let element = document.getElementById(this.parentId);
+        element.appendChild(para);
+    }
 }
-let x = new newTxtElem("div", "id0", "hi there x!", "red", "body");
-let x1 = new newTxtElem("p", "id1", "hi there x1!", "coral", "id0");
 
-x.makeElem();
-x1.makeElem();
+class newTxtElem extends newElem {
+    text: string;
+
+    constructor(type: string, idE: string, parentId: string, text: string) {
+        super(type, idE, parentId);
+        this.text = text;
+         }
+
+    makeElem(type: string,
+             idE: string,
+             parentId: string,
+             text: string) {
+        let para = document.createElement(this.type);
+        let node = document.createTextNode(this.text);
+        para.appendChild(node);
+        para.id = this.idE;
+        let element = document.getElementById(this.parentId);
+        element.appendChild(para);
+    }
+}
+
+
+
+let x = new newElem("div", "id0", "body");
+let x1 = new newTxtElem("p", "id1", "id0", "asd");
+let x2 = new newTxtElem("p", "id2", "id0", "asd");
+x.makeElem("div", "id0", "body");
+x1.makeElem("p", "id1", "id0", "asd");
+x2.makeElem("p", "id2", "id0", "asd2");
+x1.paintToColor("orange");
